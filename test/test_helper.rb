f8ada49b69d554b413487031443de4e158e70f75
Zach Machuca <zachmachuca@gmail.com>
2014-11-21 07:05:44 +0000

    Finish layout and routes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,9 +5,8 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
-  # order.
   fixtures :all
+  include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
 end
