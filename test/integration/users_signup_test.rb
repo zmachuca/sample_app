1c46718d5f6adc0a395e6731015e3f8dbda7c9d4
Zach Machuca <zachmachuca@gmail.com>
2014-11-29 20:38:16 +0000

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,6 +24,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
-    assert_not flash.empty?
+    assert is_logged_in?
   end
 end
