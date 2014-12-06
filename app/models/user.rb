9a81a44b076e3bb1cf27b2f808c81e0201eaff61
Zach Machuca <zachmachuca@gmail.com>
2014-12-06 19:16:44 +0000

    Edit models/user

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,8 +52,7 @@ class User < ActiveRecord::Base
     
     # Activates an account.
   def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: FILL_IN, activated_at: FILL_IN)
   end
 
   # Sends activation email.
@@ -64,18 +63,13 @@ class User < ActiveRecord::Base
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest:  FILL_IN,
+                   reset_sent_at: FILL_IN)
   end
 
   # Sends password reset email.
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
-  end
-  
-  # Returns true if a password reset has expired.
-  def password_reset_expired?
-    reset_sent_at < 2.hours.ago
   end
 
   private
